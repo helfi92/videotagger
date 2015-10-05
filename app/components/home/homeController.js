13efc1e6e4d57d86e93429a7c6b245df9d1285a5
Hassan Ali <helfi92@gmail.com>
2015-10-05 11:08:11 -0400

    add tag to different video

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,39 +1,70 @@
 app.controller('homeController',['$scope','$rootScope','Auth','$firebaseArray','$timeout', function($scope,$rootScope,Auth,$firebaseArray,$timeout){
 
+
+  	console.log('link is: ', $scope.urlLink);
+
   	var ref = new Firebase("https://flickering-heat-6138.firebaseio.com");
-  	var refLink = ref.child('link');
   	var refTag = ref.child('tag');
 	var refChapters = ref.child('chapters');
-
+	var refLink = ref.child('link');
 	
-
+	$scope.urlLink = '//www.youtube.com/watch?v=oAtjf6Ijmtw';
   	$scope.messages = $firebaseArray(refLink);
   	$scope.tag = $firebaseArray(refTag);
 	$scope.chapters = $firebaseArray(refChapters);
 	$scope.chaptersInObject = [];
 	
 	refChapters.on('child_added',function(snapshot){
-		console.log('all is: ', snapshot.val())
 		$scope.chaptersInObject.push(snapshot.val());
 	})
 
 	$scope.goButtonClicked = function(url){
-		console.log('go button clicked');
-		angular.element(document.querySelector('#vid1'))[0].player.src(url);
+		var player = videojs('vid1');
+		player.src(url);
+		player.bigPlayButton.show();
+		
+		player.markers.removeAll();
+
+		refTag = "";
+		refTag = ref.child('tag');
+
+		
+
+		refTag.on('child_added',function(childSnapShot){
+		
+		//if the snap has the same url, add the marker
+		
+		
+		var object = childSnapShot.val();
+		if(object.link == $scope.urlLink){
+			var time = object.starttime;
+			var goTotime = stringToMilliseconds(time);
+
+			var tagName = object.tag;
+			player.markers.add([{ time: goTotime, text: tagName}]);
+		}else{
+		}
+		
+
+
+		});
+
 	};
 	
 	$scope.writeLinkToDatabase = function(url){
-			if($rootScope.user){
+			//if($rootScope.user){
 				$scope.messages.$add({
 				url : url,
 				});
-			}else{
+			//}else{
 				console.log('Not logged in, cant write to database');
-			}		
+			//}		
 	};
-	$scope.addTag = function(chapter,tag,starttime,endtime){
+	$scope.addTag = function(chapter,tag,starttime,endtime,link,annotation){
 			//if($rootScope.user){
 				$scope.tag.$add({
+				link : link,
+				annotation : annotation,
 				chapter : chapter,
 				tag : tag,
 				starttime : starttime,
@@ -41,16 +72,24 @@ app.controller('homeController',['$scope','$rootScope','Auth','$firebaseArray','
 				});
 			// }else{
 			// 	console.log('Not logged in, cant write to database');
-			// }	
+			// }
+
+			
 	}
-	$scope.setCurrentTime = function(time){
+
+	function stringToMilliseconds (time){
 		var strTimeArray = time.split(":");
 		var seconds = parseInt(strTimeArray[0],10) * 60;
 		var ms = parseInt(strTimeArray[1],10);
 
 
 		var goTotime = seconds + ms;
-		console.log('timeInms: ', goTotime);
+		return goTotime;
+	}
+	
+	$scope.setCurrentTime = function(time){
+		var goTotime = stringToMilliseconds(time);
+		
 		document.getElementById('vid1').player.currentTime(goTotime);
 	}
 
@@ -59,6 +98,34 @@ app.controller('homeController',['$scope','$rootScope','Auth','$firebaseArray','
 			name : chapter
 		});
 	};
+
+	// This is for the when you first open the window, the default video shown
+	refTag.on('child_added',function(childSnapShot){
+		
+		//if the snap has the same url, add the marker
+		
+		var player = videojs('vid1');
+		var object = childSnapShot.val();
+		if(object.link == $scope.urlLink){
+			var time = object.starttime;
+			var goTotime = stringToMilliseconds(time);
+
+			var tagName = object.tag;
+
+			player.markers.add([{ time: goTotime, text: tagName}]);
+
+
+		}else{
+		}
+		
+
+
+	});
+
+	
+	
+
+	
 
 	$scope.init = function(){
 		videojs('vid1', {
@@ -69,18 +136,28 @@ app.controller('homeController',['$scope','$rootScope','Auth','$firebaseArray','
 	          }
 	        },
 
-	      });
+	    });
 
+		
 		var player = videojs('vid1');
 		player.markers({
-		   markers: [
-		      {time: 9.5, text: "this"},
-		      {time: 16,  text: "is"},
-		      {time: 23.6,text: "so"},
-		      {time: 28,  text: "cool"}
-		   ]
+		   	markerStyle: {
+		      	'width':'7px',
+		      	'border-radius': '30%',
+		      	'background-color': 'blue'
+	   		},
+	   		markerTip:{
+		    	display: true,
+		      	text: function(marker) {
+		        return marker.text;
+		      },
+		      time: function(marker) {
+		         return marker.time;
+		      }
+		   },
+		   
 		});
-
+		//player.markers.removeAll();
 
 	}
 	$scope.init();
